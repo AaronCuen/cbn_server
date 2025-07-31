d2d1ed2b9fbe2593e27d7d617c7171069f6fe0df
AaronCuen <122289027+AaronCuen@users.noreply.github.com>
2025-07-30 18:51:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //conexion a la db
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: mysql,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
